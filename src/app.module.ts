e413a506e09ff7473225bbfa5575869aeccec00a
keonhee <kh.bae@aria-edge.com>
2023-01-16 15:41:14 +0900

    mysql setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,39 @@
 import { RedisModule } from '@liaoliaots/nestjs-redis';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SocketModule } from './socket/socket.module';
 
 @Module({
   imports: [
-    SocketModule,
-    RedisModule.forRoot({
-      config: {
-        host: 'localhost',
-        port: 26379,
-        namespace: 'user_socket',
-        db: 1,
-      },
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 23306,
+      username: 'root',
+      password: '3565',
+      database: 'random_chat',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
     }),
+    RedisModule.forRoot({
+      config: [
+        {
+          host: 'localhost',
+          port: 26379,
+          namespace: 'user_socket',
+          db: 1,
+        },
+        {
+          host: 'localhost',
+          port: 26379,
+          namespace: 'socket_user',
+          db: 2,
+        },
+      ],
+    }),
+    SocketModule,
   ],
   controllers: [AppController],
   providers: [AppService],
