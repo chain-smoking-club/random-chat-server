82a165096abab776214241d904936df430c8fe4f
fidelisbae <fidelisbae@gmail.com>
2023-02-05 17:21:40 +0900

    env

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.NESTJS_PORT);
+  await app.listen(4000);
 }
 bootstrap();
